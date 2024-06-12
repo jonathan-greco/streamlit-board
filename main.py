fd11e6168b9806ed02315f5f6121364b87936089
Jonathan Greco <47996289+jonathan-greco@users.noreply.github.com>
2024-06-12 18:44:48 -0300

    ajustes no config do main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+from io import StringIO
+
+#configs
+st.set_page_config(
+    page_title="DataBoard em StreamLit",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# DataBoard v.1.0 - Autor: Jonathan Greco"
+    }
+)
+
+st.title('Página principal')
 
 sidebar_logo = 'images/logotipo-sidebar-1.png'
 st.logo(sidebar_logo)
